c3db9680ad168c1e56460b44c36b143c81d85d65
MarceloFRSilva <marregsil97@gmail.com>
2021-01-18 11:37:51 +0100

    1st commit

---
+++ bank-system/client/src/main/java/com/bank/ClientMain.java
@@ -0,0 +1,9 @@
+package com.bank;
+
+public class ClientMain {
+
+    public static void main(final String[] args) {
+        System.out.println("Aqui!!");
+
+    }
+}
---
+++ bank-system/server/src/main/java/com/bank/ServerMain.java
@@ -0,0 +1,10 @@
+package com.bank;
+
+public class ServerMain {
+
+    public static void main(final String[] args) {
+        System.out.println("Aqui!!");
+
+    }
+
+}
